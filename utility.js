a70d7d464eb59a7305813351c2df4a82d088cb68
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-08 08:00:46 +0530

    Refactor:Added part time employee and wage

--- utility.js
+++ utility.js
@@ -1,3 +1,6 @@
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const EMP_RATE_PER_HOUR=20;
 class EmployeeWageProgram{
     employeeCheck=()=>{
       let Present = 1;
@@ -7,13 +10,26 @@ class EmployeeWageProgram{
     } else {
         console.log("employee is absent ");
     }
-  }
-    employeeDailyWage=(employeeCheck)=>{
-    const IS_FULL_TIME = 1;
-    const EMP_RATE_PER_HOUR=20;
-    let empHrs = 8;
-    let empWage = empHrs * EMP_RATE_PER_HOUR;
-    console.log("employeeWage ",empWage);
-    }
 }
+  
+    employeeDailyWage=(employeeCheck)=>{
+        let empHrs ;
+        let empWage;
+        let  empCheck = Math.floor(Math.random() * 10) % 3;
+    if (empCheck==IS_FULL_TIME) {
+        console.log("employee is full time present ");
+         empHrs = 8;
+         empWage = empHrs * EMP_RATE_PER_HOUR;
+        console.log("employeeWage ",empWage);
+    } else
+    {
+        console.log("employee is part time present  ");
+         empHrs = 4;
+         empWage = empHrs * EMP_RATE_PER_HOUR;
+        console.log("employeeWage ",empWage);
+        }
+     }
+
+    }
+
 module.exports = new EmployeeWageProgram();
